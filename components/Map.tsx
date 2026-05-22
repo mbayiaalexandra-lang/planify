b25045cc9f0d0aee816a35d04608e12ab88eae5e
mbayiaalexandra-lang <mbayiaalexandra@gmail.com>
2026-05-22 20:16:49 +0200

    simplify map for deploy

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,36 +1,21 @@
 "use client";
 
-import dynamic from "next/dynamic";
-
-const DynamicMap = dynamic(
-  async () => {
-    const { MapContainer, TileLayer, Marker, Popup } = await import("react-leaflet");
-
-    return function Map() {
-      return (
-        <MapContainer
-          center={[5.3599517, -4.0082563]}
-          zoom={13}
-          scrollWheelZoom={false}
-          style={{ height: "400px", width: "100%" }}
-        >
-          <TileLayer
-            attribution='&copy; OpenStreetMap contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-
-          <Marker position={[5.3599517, -4.0082563]}>
-            <Popup>Salon Beauté CI</Popup>
-          </Marker>
-        </MapContainer>
-      );
-    };
-  },
-  {
-    ssr: false,
-  }
-);
-
-export default DynamicMap;
- 
-//update
+export default function Map() {
+  return (
+    <div
+      style={{
+        height: "400px",
+        width: "100%",
+        borderRadius: "24px",
+        background: "#e5e7eb",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        color: "#111827",
+        fontWeight: 700,
+      }}
+    >
+      Carte des salons bientôt disponible
+    </div>
+  );
+}
